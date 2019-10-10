615b08aedf024b9e65564726a9d3420873583dab
realExecutorg <56406482+realExecutorg@users.noreply.github.com>
2019-10-10 23:57:59 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/1rkzkd2z6i8hn7n/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/pxg6wkwki42x5g9/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['elecom', 'explorer', 'logitech']
